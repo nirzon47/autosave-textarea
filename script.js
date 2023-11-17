65b49f54ff1d91b4d172e6473a54622d361024b6
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-17 19:41:31 +0530

    added theme control

--- script.js
+++ script.js
@@ -9,13 +9,32 @@ const theme = localStorage.getItem('theme') || 'dark'
 // Event listener
 document.addEventListener('DOMContentLoaded', () => {
 	textAreaInit()
+	themeInit()
 })
 
 textAreaElement.addEventListener('input', () => {
 	localStorage.setItem('text', textAreaElement.value)
 })
 
+themeControllerElement.addEventListener('change', () => {
+	if (themeControllerElement.checked) {
+		localStorage.setItem('theme', 'light')
+		document.documentElement.setAttribute('data-theme', 'light')
+	} else {
+		localStorage.setItem('theme', 'dark')
+		document.documentElement.setAttribute('data-theme', 'dark')
+	}
+})
+
 // Functions
 const textAreaInit = () => {
 	textAreaElement.value = savedText
+}
+
+const themeInit = () => {
+	if (theme === 'light') {
+		themeControllerElement.checked = true
+	} else {
+		themeControllerElement.checked = false
+	}
 }
